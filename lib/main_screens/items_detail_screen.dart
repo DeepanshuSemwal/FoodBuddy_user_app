ccee96dbecd2ae4d6d398656aebc8c74829da271
Deepanshu <semwaldeepanshu@gmail.com>
2022-12-08 17:09:10 +0530

    10th

--- lib/main_screens/items_detail_screen.dart
+++ lib/main_screens/items_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
 import 'package:wow_food_user_app/models/items.dart';
 import 'package:wow_food_user_app/widgets/custum_app_bar.dart';
 
@@ -12,9 +13,62 @@ class ItemDetialScreen extends StatefulWidget {
 }
 
 class _ItemDetialScreenState extends State<ItemDetialScreen> {
+
+  TextEditingController counterController=TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return CustumAppBar();
+    return Scaffold(
+      appBar: CustumAppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+         Container(
+           child:Image.network(widget.model!.thumbnail.toString(),
+             fit: BoxFit.fill,
+
+           ),
+           height: 300,
+           width: MediaQuery.of(context).size.width,
+
+         ),
+
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: NumberInputPrefabbed.roundedButtons(
+
+              controller: counterController,
+              incDecBgColor: Colors.red,
+              min: 1,
+              max: 100,
+              initialValue: 1,
+              buttonArrangement: ButtonArrangement.incRightDecLeft,
+
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              widget.model!.itemTitle.toString(),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 21,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              widget.model!.itemDescription.toString(),
+              style: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 15,
+              ),
+            ),
+          ),
+        ],
+      ),
+
+    );
   }
 }
 
